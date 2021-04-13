a19d0cad8e9a8e26a1cf67d0d069aee4fd4b7c6d
djdsdjds <42968335+djdsdjds@users.noreply.github.com>
2021-04-13 13:50:10 +0100

    Add files via upload
    printf e rad

--- ballAlg.cpp
+++ ballAlg.cpp
@@ -1,25 +1,16 @@
 
-#include <array>
 #include <math.h>
-#include <algorithm>
-#include <random>
-#include <iostream>
-#include <limits>
-#include <string>
+
 #include <stdlib.h>
 
 #include "gen_points.c"
 
-using std::vector;
-using std::cout;
-using std::endl;
 
 /*
   falta:
   limpar memoria
   substituir no void a 2 chamada do orth por uma copia
   melhorar alguma das alocaçºoes de memoria que fiz
-  testar com o codigo do professor
   */
 struct tree{
     double* center;
@@ -36,14 +27,14 @@ struct pair_int{
 
 
 
-size_t n_dim;
-size_t id=0;
+long n_dim;
+long id=0;
 
 
 double eucl(double *aux1, double *aux2){
     double ret=0.0;
     double aux=0.0;
-    for(size_t i=0; i < n_dim; i++){
+    for(long i=0; i < n_dim; i++){
         aux=aux1[i]-aux2[i];
         ret= ret+ aux*aux;
     }
@@ -52,7 +43,7 @@ double eucl(double *aux1, double *aux2){
 
 double inner(double *a, double *b){
     double ret=0.0;
-    for(size_t i=0;i < n_dim; i++){
+    for(long i=0;i < n_dim; i++){
         ret=ret+a[i]*b[i];
     }
     return ret;
@@ -60,7 +51,7 @@ double inner(double *a, double *b){
 
 double * add( double *aux1, double *aux2){
     double *arr = (double *) malloc(n_dim * sizeof(double));
-    for(size_t i=0;i < n_dim; i++){
+    for(long i=0;i < n_dim; i++){
         arr[i]=aux1[i]+aux2[i];
     }
     return arr;
@@ -68,7 +59,7 @@ double * add( double *aux1, double *aux2){
 
 double * sub( double *aux1, double *aux2){
     double *arr = (double *) malloc(n_dim * sizeof(double));
-    for(size_t i=0;i < n_dim; i++){
+    for(long i=0;i < n_dim; i++){
         arr[i]=aux1[i]-aux2[i];
     }
     return arr;
@@ -76,18 +67,18 @@ double * sub( double *aux1, double *aux2){
 
 double * mult( double aux1, double *aux2){
     double *arr = (double *) malloc(n_dim * sizeof(double));
-    for(size_t i=0;i < n_dim; i++){
+    for(long i=0;i < n_dim; i++){
         arr[i]=aux1*aux2[i];
     }
     return arr;
 }
 
-double ** orth(double **dataset, pair_int a_b, size_t data_size){
+double ** orth(double **dataset, pair_int a_b, long data_size){
 
     double **ret = (double **) malloc(data_size * sizeof(double *));
 
 
-    for(size_t i = 0; i < data_size; i++) ret[i] = (double *)malloc(n_dim * sizeof(double));
+    for(long i = 0; i < data_size; i++) ret[i] = (double *)malloc(n_dim * sizeof(double));
 
     double *b_a = (double *) malloc(n_dim * sizeof(double));
 
@@ -95,12 +86,11 @@ double ** orth(double **dataset, pair_int a_b, size_t data_size){
 
     double inner_b_a=inner(b_a,b_a);
 
-    for(size_t i=0;i < data_size ;i++){
+    for(long i=0;i < data_size ;i++){
 
 
         ret[i]= add(mult((inner( sub(dataset[i],dataset[a_b.first]) ,b_a )/ inner_b_a),b_a),dataset[a_b.first]);
 
-        // n sei se perferem melhorar como isto está escrito
     }
 
     return ret;
@@ -108,12 +98,12 @@ double ** orth(double **dataset, pair_int a_b, size_t data_size){
 }
 
 
-pair_int far_away(double **data, size_t size){
+pair_int far_away(double **data, long size){
     //metedo descrito no enunciado
 
     double aux= 0;
     int a=0;
-    for(size_t i=1;i<size;i++){
+    for(long i=1;i<size;i++){
         double auxx=eucl(data[0],data[i]);
             if(auxx>aux){
                 aux=auxx;
@@ -122,7 +112,7 @@ pair_int far_away(double **data, size_t size){
     }
     int b=0;
     aux=0;
-        for(size_t j=0;j<size;j++){
+        for(long j=0;j<size;j++){
 
         double auxx=eucl(data[a],data[j]);
             if(auxx>aux){
@@ -152,7 +142,7 @@ int comp(const void *a, const void *b){ // Não está a entrar aqui ???
         return 0;
 }
 
-double* median_center( double **orth,size_t size){
+double* median_center( double **orth,long size){
 
 
         // n sei se isto modifica a ordem
@@ -162,7 +152,7 @@ double* median_center( double **orth,size_t size){
 
     if (size%2 == 0){
         double *ret = (double *) malloc(n_dim * sizeof(double));
-        for (size_t i = 0; i < n_dim; i++){
+        for (long i = 0; i < n_dim; i++){
             ret[i]= (orth[size/2][i] + orth[size/2-1][i])/2;
 
         }
@@ -175,36 +165,32 @@ double* median_center( double **orth,size_t size){
 struct L_R_ret{
     double** first;
     double** second;
-    size_t first_size;
-    size_t second_size;
+    long first_size;
+    long second_size;
 };
 
-L_R_ret L_R(double ** data,double ** orthg,double* center,size_t data_size){
-
+L_R_ret L_R(double ** data,double ** orthg,double* center,long data_size){
 
     double **ret1 = (double **) malloc(data_size * sizeof(double *));
 
-    for(size_t i = 0; i < data_size; i++) ret1[i] = (double *)malloc(n_dim * sizeof(double));
+    for(long i = 0; i < data_size; i++) ret1[i] = (double *)malloc(n_dim * sizeof(double));
 
 
-    double **ret2 = (double **) malloc((data_size) * sizeof(double *));
-    for(size_t i = 0; i < data_size; i++) ret2[i] = (double *)malloc(n_dim * sizeof(double));
+    double **ret2 = (double **) malloc(data_size * sizeof(double *));
+    for(long i = 0; i < data_size; i++) ret2[i] = (double *)malloc(n_dim * sizeof(double));
 
     int aux1=0;
     int aux2=0;
-
-    for(size_t i=0;i<data_size;i++){
-
+    for(long i=0;i<data_size;i++){
         if(orthg[i][0]<center[0]){
-
-            for(size_t j=0; j<n_dim;j++){
+            for(long j=0; j<n_dim;j++){
                 ret1[aux1][j]=data[i][j];
             }
             aux1++;
         }
 
         else{
-            for(size_t j=0; j<n_dim;j++){
+            for(long j=0; j<n_dim;j++){
                 ret2[aux2][j]=data[i][j];
 
             }
@@ -217,28 +203,27 @@ L_R_ret L_R(double ** data,double ** orthg,double* center,size_t data_size){
     L_R_ret ret;
     ret.first=(ret1);
     ret.second=(ret2);
-    ret.first_size=(aux1);
-    ret.second_size=(aux2);
-    cout<< ret.first_size << " "<< ret.second_size << endl;
+    ret.first_size=aux1;
+    ret.second_size=aux2;
 
     return ret;
 
 
 }
-double rad(double** data, double* center,size_t data_size){
+double rad(double** data, double* center,long data_size){
     double ret=0;
-    for(size_t i=0; i<data_size;i++){
+    for(long i=0; i<data_size;i++){
         double aux=eucl(data[i],center);
         if(aux>ret){
             ret=aux;
         }
 
     }
-    return pow(ret,0.5);
+    return sqrt(ret);
 }
 
 
-void fit(tree *node, double** dataset, size_t size){
+void fit(tree *node, double** dataset, long size){
 
     node->id=id;
     id++;
@@ -251,8 +236,6 @@ void fit(tree *node, double** dataset, size_t size){
         node->R->id=-1;
     }
     else{
-
-
     pair_int a_b=far_away(dataset, size);
 
     double **orth_aux = orth(dataset, a_b, size);
@@ -266,10 +249,7 @@ void fit(tree *node, double** dataset, size_t size){
 
     L_R_ret L_R_aux= L_R(dataset,orth_aux,node->center,size);
 
-    free(dataset);
-    free(orth_aux);
     node->L= new tree;
-
     fit(node->L,L_R_aux.first,L_R_aux.first_size);
     node->R= new tree;
     fit(node->R,L_R_aux.second,L_R_aux.second_size);
@@ -281,12 +261,15 @@ void fit(tree *node, double** dataset, size_t size){
 
 // Funcao Visit
 void visit(tree *node) {
-  cout<<node->id << " "<<node->L->id <<" "<<node->R->id << " "<< node->rad <<" ";
-  for(int i=0; i<n_dim-1;i++){
 
-      cout<<node->center[i]<<" ";
+  printf("%d %d %d %f ",node->id,node->L->id,node->R->id, node->rad);
+
+  for(int i=0; i<n_dim-1;i++){
+      printf("%f ",node->center[i]);
+
   }
-  cout<<node->center[n_dim-1] << endl;
+
+  printf("%f\n",node->center[n_dim-1]);
 }  // funcao que imprime um produto da arvore
 
 void traverse(tree *node) {
@@ -315,11 +298,13 @@ int main(int argc, char *argv[]){
     long np = atol(argv[2]);
 
     double **data = get_points(argc, argv, &n_dim_aux, &np);
-    n_dim=(size_t)n_dim_aux;
-    tree* aux= new tree;   
+    n_dim=n_dim_aux;
+    tree* aux= new tree;
 
     fit(aux,data, np);
-    cout<<n_dim_aux<<" "<<id<< endl;
+
+    printf("%d %ld\n",n_dim_aux,id);
+
     traverse(aux);
     return 0;
 }
