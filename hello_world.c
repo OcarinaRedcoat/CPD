c52b28d842d7d00c77a66ec898c14ceb5e976fd5
djdsdjds <42968335+djdsdjds@users.noreply.github.com>
2021-04-28 11:30:18 +0100

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+#include <mpi.h>
+#include <stdio.h>
+
+main(int argc, char *argv[]){
+  int me, nprocs;
+  
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &me);
+  
+  printf("Task %d sends greatings from machine %d \n", nprocs, me);
+  
+  MPI_Finalize();
+  
+} 
